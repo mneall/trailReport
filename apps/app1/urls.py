3ef70932ab52635beddaed92616e003fa60e1e73
Matthew Neall <neallmr@gmail.com>
2016-09-27 13:46:19 -0700

    add static files

--- apps/app1/urls.py
+++ apps/app1/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
