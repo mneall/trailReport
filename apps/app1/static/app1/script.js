2d0f62a5874820127865ff1146997205f63e0351
Matthew Neall <neallmr@gmail.com>
2016-09-29 12:18:00 -0700

    add infowindows to markers with html form, change icons to PNGs

--- apps/app1/static/app1/script.js
+++ apps/app1/static/app1/script.js
@@ -1,20 +1,24 @@
-// $(document).ready(function(){
  		
-	var map, bounds, projection, zoom;
+	var map, infowindow, bounds, projection, zoom;
 
   	function initMap(callback) {
-  		var marker;
     	map = new google.maps.Map(document.getElementById('map'), {
-      	zoom: 8,
-      	center: {lat: 47.608013, lng: -122.335167}
+      		zoom: 8,
+      		center: {lat: 47.608013, lng: -122.335167}
       	// TODO: add infowindow to markers, add custom markers
     	});
 
-    	marker = new google.maps.Marker({
-			map: map,
-			draggable: true,
-			animation: google.maps.Animation.DROP,
-			position: {lat: 47.608013, lng: -122.335167}
+    	var windowForm = "<table>" +
+                 "<tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>" +
+                 "<tr><td>Address:</td> <td><input type='text' id='address'/></td> </tr>" +
+                 "<tr><td>Type:</td> <td><select id='type'>" +
+                 "<option value='bar' SELECTED>bar</option>" +
+                 "<option value='restaurant'>restaurant</option>" +
+                 "</select> </td></tr>" +
+                 "<tr><td></td><td><input type='button' value='Save & Close' onclick='saveData()'/></td></tr>";
+		
+		infowindow = new google.maps.InfoWindow({
+     		content: windowForm
     	});
 
     	google.maps.event.addListenerOnce(map, 'bounds_changed', function(){
@@ -23,13 +27,12 @@
     		zoom = (this.getZoom());
 			});
 
-    	google.maps.event.addListenerOnce(map, 'idle', function(){
+       	google.maps.event.addListenerOnce(map, 'idle', function(){
     		callback();
-			});
+		});
   	}
 
-	function pixelToLatLng (map, x, y) {
-		var projection = map.getProjection();
+	function pixelToLatLng (map, projection, x, y) {
 		var topRight = projection.fromLatLngToPoint(map.getBounds().getNorthEast());
 		var bottomLeft = projection.fromLatLngToPoint(map.getBounds().getSouthWest());
 		var scale = 1 << map.getZoom();
@@ -39,23 +42,30 @@
 	$("#map").droppable({
 	    accept: ".draggable",
 	    drop: function(event,ui){
-	    	var wrapper = $("#map");
-	        var map_div_pos = wrapper.offset();
-	        var map_div_border_left = parseInt(wrapper.css("border-left-width"),10);
-	        var map_div_border_top = parseInt(wrapper.css("border-top-width"),10);
+	    		var wrapper = $("#map");
+	        	var map_div_pos = wrapper.offset();
+	        	var map_div_border_left = parseInt(wrapper.css("border-left-width"),10);
+	        	var map_div_border_top = parseInt(wrapper.css("border-top-width"),10);
+	        	var mouse_pos = ui.helper.offset();
+	        	var x = mouse_pos.left - map_div_border_left - map_div_pos.left;
+	        	var y = mouse_pos.top - map_div_border_top - map_div_pos.top;
+	        	lat_lng = pixelToLatLng(map, projection, x, y);
 
-	        var mouse_pos = ui.helper.offset();
-	        var x = mouse_pos.left - map_div_border_left - map_div_pos.left;
-	        var y = mouse_pos.top - map_div_border_top - map_div_pos.top;
-	        lat_lng = pixelToLatLng(map, x, y);
-
-	   		google.maps.add_group_marker = new google.maps.Marker(
+	   		var marker = new google.maps.Marker(
 	        {
 	            position: lat_lng,
 	            draggable: true,
 	            map: map,
+	            label: "X",
 	            zIndex: Math.round(lat_lng.lat()*-100000)<<5
 	        });
+
+			google.maps.event.addListener(marker, "click", function() {
+				infowindow.open(map, marker);
+			});
+
+			// google.maps.add_group_marker = marker;
+
 	    }
 	});
 
@@ -63,13 +73,11 @@
     {
         helper: "clone",
         appendTo: $('#map'),
-        cursorAt: { left: 5, top: 5 },
+        cursorAt: { left: 10, top: 0 },
         stop: function(event, ui)
         {
  		// TODO: make icon show after picking up and before getting to map div
         }
     });
 
-    // initMap();
-// });
  
